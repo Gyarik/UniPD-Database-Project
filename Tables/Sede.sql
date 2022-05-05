a9f5d1133318f9d4bb9fa82afae6d2b43bfd2851
Gyarik <alex64.AT93@gmail.com>
2022-05-05 11:30:45 +0200

    add data to Sede

--- Tables/Sede.sql
+++ Tables/Sede.sql
@@ -2,14 +2,23 @@ DROP TABLE IF EXISTS Sede;
 
 CREATE TABLE Sede (
 	id_sede INT NOT NULL,
-	n_civico VARCHAR(20) NOT NULL,
+	n_civico CHAR(5) NOT NULL,
 	via VARCHAR(20) NOT NULL,
 	citta VARCHAR(20) NOT NULL,
-	provincia VARCHAR(20) NOT NULL,
+	provincia CHAR(2) NOT NULL,
 	cap CHAR(5) NOT NULL,
 	PRIMARY KEY (id_sede)
 );
 
 INSERT INTO Sede(id_sede, n_civico, via, citta, provincia, cap)
 values
-	(1, '81', 'Ospedale Civile', 'Padova', 'Padova', '35121');
+	(389, '81', 'Via Ospedale Civile', 'Padova', 'PD', '35121'),
+	(437, '9', 'Via Tumedei Casalis', 'Carmagnola', 'TO', '10022'),
+	(594, '74', 'Via Delle Pianazze', 'La Spezia', 'SP', '19136'),
+	(239, '57', 'Via Torelli', 'Prato', 'PO', '59100'),
+	(312, '2', 'Via Delle Gagliarde', 'Arezzo', 'AR', '52100'),
+	(814, '18', 'Viale Suzzani', 'Milano', 'MI', '20162'),
+	(641, '20', 'Via Caretto', 'Baveno', 'VB', '28831'),
+	(933, '101', 'Via Donatello', 'Cislago', 'VA', '21040'),
+	(192, '40/H', 'Via IV Novembre', 'Modena', 'MO', '41123'),
+	(284, '2', 'Via Carlo Poma', 'Roma', 'RM', '00195');
