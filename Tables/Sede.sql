29072a861e960def37b21c3a15f1171153db130e
Gyarik <alex64.AT93@gmail.com>
2022-05-05 15:31:00 +0200

    reduce data amount

--- Tables/Sede.sql
+++ Tables/Sede.sql
@@ -16,9 +16,4 @@ values
 	(437, '9', 'Via Tumedei Casalis', 'Carmagnola', 'TO', '10022'),
 	(594, '74', 'Via Delle Pianazze', 'La Spezia', 'SP', '19136'),
 	(239, '57', 'Via Torelli', 'Prato', 'PO', '59100'),
-	(312, '2', 'Via Delle Gagliarde', 'Arezzo', 'AR', '52100'),
-	(814, '18', 'Viale Suzzani', 'Milano', 'MI', '20162'),
-	(641, '20', 'Via Caretto', 'Baveno', 'VB', '28831'),
-	(933, '101', 'Via Donatello', 'Cislago', 'VA', '21040'),
-	(192, '40/H', 'Via IV Novembre', 'Modena', 'MO', '41123'),
-	(284, '2', 'Via Carlo Poma', 'Roma', 'RM', '00195');
+	(312, '2', 'Via Delle Gagliarde', 'Arezzo', 'AR', '52100');
