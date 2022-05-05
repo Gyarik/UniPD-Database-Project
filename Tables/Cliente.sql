5a3d06f91228e79a33291d920a5f6e1a0bb0c82f
Samuel Scarabottolo <samuel@Air-di-Samuel.station>
2022-05-05 17:53:47 +0200

    aggiunta Cliente.sql

---
+++ Tables/Cliente.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS Cliente;
+
+CREATE TABLE Cliente (
+    p_iva VARCHAR(11) NOT NULL,
+    nome_azienda VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Cliente(p_iva, nome_azienda)
+VALUES
+    ('26585340701', 'Idro Software'),
+    ('76630840890', 'Software Guru'),
+    ('79833500295', 'Immobili Compatto'),
+    ('18727650444', 'Deutch Reporter'),
+    ('25811980355', 'Nebula Zurich'),
+    ('22337910206', 'Well Food'),
+    ('56720480328', 'ACE Chemicals'),
+    ('48503850165', 'Super Track'),
+    ('70779410680', 'Marte Progetti'),
+    ('38481280865', 'Falcone Industries')
+
