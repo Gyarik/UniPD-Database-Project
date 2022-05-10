f6a93d26686a81f2f04faa5dea58ab7052cd54d5
Gyarik <alex64.AT93@gmail.com>
2022-05-10 16:07:24 +0200

    fix implementation error

--- Tables/Retribuzioni.sql
+++ Tables/Retribuzioni.sql
@@ -3,52 +3,50 @@ DROP TABLE IF EXISTS Retribuzioni;
 CREATE TABLE Retribuzioni (
     id_trz INT NOT NULL,
     id_dip INT NOT NULL,
-    id_sede INT NOT NULL,
-    PRIMARY KEY (id_trz, id_dip, id_sede),
+    PRIMARY KEY (id_trz, id_dip),
 	FOREIGN KEY (id_trz) REFERENCES Transazione(id_trz),
-    FOREIGN KEY (id_dip) REFERENCES Dipendente(id_dip),
-    FOREIGN KEY (id_sede) REFERENCES Transazione(id_sede)
+    FOREIGN KEY (id_dip) REFERENCES Dipendente(id_dip)
 );
 
-INSERT INTO Retribuzioni (id_trz, id_dip, id_sede)
+INSERT INTO Retribuzioni (id_trz, id_dip)
 VALUES
 
     --Dipendenti della sede 389 
-    (19, 212, 389),
-    (20, 119, 389),
-    (21, 690, 389),
-    (22, 333, 389),
-    (23, 787, 389),
-    (24, 173, 389),
+    (19, 212),
+    (20, 119),
+    (21, 690),
+    (22, 333),
+    (23, 787),
+    (24, 173),
 
     --Dipendenti della sede 437
-    (43, 423, 437),
-    (44, 777, 437),
-    (45, 111, 437),
-    (46, 677, 437),
-    (47, 123, 437),
-    (48, 347, 437),
+    (43, 423),
+    (44, 777),
+    (45, 111),
+    (46, 677),
+    (47, 123),
+    (48, 347),
 
     --Dipendenti della sede 594
-    (67, 675, 594),
-    (68, 723, 594),
-    (69, 309, 594),
-    (70, 334, 594),
-    (71, 432, 594),
-    (72, 789, 594),
+    (67, 675),
+    (68, 723),
+    (69, 309),
+    (70, 334),
+    (71, 432),
+    (72, 789),
 
     --Dipendenti della sede 239
-    (91, 764, 239),
-    (92, 609, 239),
-    (93, 543, 239),
-    (94, 124, 239),
-    (95, 444, 239),
-    (96, 989, 239),
+    (91, 764),
+    (92, 609),
+    (93, 543),
+    (94, 124),
+    (95, 444),
+    (96, 989),
 
     --Dipendenti della sede 312
-    (115, 234, 312),
-    (116, 420, 312),
-    (117, 890, 312),
-    (118, 999, 312),
-    (119, 998, 312), 
-    (120, 511, 312);
+    (115, 234),
+    (116, 420),
+    (117, 890),
+    (118, 999),
+    (119, 998), 
+    (120, 511);
