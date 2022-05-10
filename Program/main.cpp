45a8a540047e74360e42fd44947b75b53b5a2687
Gyarik <alex64.AT93@gmail.com>
2022-05-10 14:31:23 +0200

    add pretty print

--- Program/main.cpp
+++ Program/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 PGconn* tryConn(const char* host, const char* user, const char* db, const char* pass, const char* port);
 void checkResults(PGresult* res, const PGconn* conn);
+void dividePrint(int fields, int* maxLen);
+void queryPrint(PGresult* res);
 
 int main(int argc, char* argv[]) {
     if(argc < 6) {
@@ -20,18 +22,8 @@ int main(int argc, char* argv[]) {
     PGresult* res = PQexec(conn, "SELECT * FROM progetto");
     checkResults(res, conn);
 
-    int tuples = PQntuples(res);
-    int fields = PQnfields(res);
-
-    for(int i=0; i<fields; i++)
-        cout << PQfname(res, i) << "\t\t";
-    cout << endl;
-
-    for(int i=0; i<tuples; i++) {
-        for(int j=0; j<fields; j++)
-            cout << PQgetvalue(res, i, j) << "\t\t";
-        cout << endl;
-    }
+    // Print placeholder query
+    queryPrint(res);
 
     PQclear(res);
     PQfinish(conn);
@@ -63,4 +55,65 @@ void checkResults(PGresult* res, const PGconn* conn) {
         PQclear(res);
         exit(1);
     }
+}
+
+void dividePrint(int fields, int* maxLen) {
+    for(int i=0; i<fields; i++) {
+        cout << '+';
+        for(int j=0; j<maxLen[i]+2; j++)
+            cout << '-';
+    }
+    cout << '+' << endl;
+}
+
+void queryPrint(PGresult* res) {
+    // n. tuple
+    int tuples = PQntuples(res);
+    // n. campi
+    int fields = PQnfields(res);
+    // array di supporto per salvare le colonne della query
+    string query[tuples + 1][fields];
+
+    for(int i=0; i<fields; i++)
+        query[0][i] = PQfname(res, i);
+
+    for(int i=0; i<tuples; i++) {
+        for(int j=0; j<fields; j++)
+            query[i+1][j] = PQgetvalue(res, i, j);
+    }
+
+    // Trova l'elemento piu' lungo per ogni colonna
+    int maxLen[fields];
+    for(int i=0; i<fields; i++) {
+        maxLen[i] = 0;
+        for(int j=0; j<tuples+1; j++) {
+            int size = query[j][i].size();
+            maxLen[i] = size > maxLen[i] ? size : maxLen[i];
+        }
+    }
+
+    // Stampa il risultato della query
+    dividePrint(fields, maxLen);
+    for(int i=0; i<fields; i++) {
+        cout << "| ";
+        cout << query[0][i];
+        for(int j=0; j<maxLen[i]-query[0][i].size()+1; j++)
+            cout << ' ';
+        if(i == fields-1)
+            cout << '|';
+    }
+    cout << endl;
+    dividePrint(fields, maxLen);
+    for(int i=1; i<tuples+1; i++) {
+        for(int j=0; j<fields; j++) {
+            cout << "| ";
+            cout << query[i][j];
+            for(int k=0; k<maxLen[j]-query[i][j].size()+1; k++)
+                cout << ' ';
+            if(j == fields-1)
+                cout << '|';
+        }
+        cout << endl;
+    }
+    dividePrint(fields, maxLen);
 }
