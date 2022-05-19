e56250a6dd3759ff1b3149494a969ffcf3649f59
Gyarik <alex64.AT93@gmail.com>
2022-05-19 17:43:47 +0200

    add query

--- Queries/Query1.sql
+++ Queries/Query1.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(*) as moduli, dipendente.id_dip, cognome, nome, citta, provincia
 FROM assegnazione JOIN dipendente ON assegnazione.id_dip = dipendente.id_dip
-				  JOIN sede ON dipendente.id_sede = sede.id_sede
+JOIN sede ON dipendente.id_sede = sede.id_sede
 WHERE provincia = 'PO'
 GROUP BY dipendente.id_dip, cognome, nome, citta, provincia
 ORDER BY moduli DESC
