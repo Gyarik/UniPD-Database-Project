7144f63a36f5dd0cc1f533d0f93931742c4e50f7
Samuel Scarabottolo <samuel@Air-di-Samuel.station>
2022-05-26 13:21:32 +0200

    fixed Query8

--- Queries/Query8.sql
+++ Queries/Query8.sql
@@ -1,8 +1,8 @@
 --Nome dei progetti e moduli in cui vi lavorano 2 o più dipendenti
 
 
-SELECT p.nome_prog, a.id_modulo
+SELECT p.nome_prog, a.id_modulo, COUNT(*) as numero_dip
 FROM Progetto as p JOIN Modulo as m ON p.id_prog = m.id_prog
     JOIN Assegnazione as a ON a.id_modulo = m.id_modulo
 GROUP BY p.nome_prog, a.id_modulo
-HAVING COUNT(*) >= 2;
+HAVING COUNT(*) >= 3;
